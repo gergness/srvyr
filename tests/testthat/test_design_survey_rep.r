8dd29caa54a480dc44ea23df2da08956f5f11fa6
gergness <greg.freedman@gmail.com>
2015-10-27 21:03:40 -0500

    Finish testing design_survey_rep()

--- tests/testthat/test_design_survey_rep.r
+++ tests/testthat/test_design_survey_rep.r
@@ -4,23 +4,15 @@ library(srvyr)
 # From survey::as.svrepdesign examples
 library(survey)
 data(scd)
-scddes<-svydesign(data=scd, prob=~1, id=~ambulance, strata=~ESA,
-                  nest=TRUE, fpc=rep(5,6))
-scdnofpc<-svydesign(data=scd, prob=~1, id=~ambulance, strata=~ESA,
-                    nest=TRUE)
+scddes<-svydesign(data = scd, prob = ~1, id = ~ambulance, strata = ~ESA,
+                  nest = TRUE, fpc = rep(5, 6))
+scdnofpc<-svydesign(data = scd, prob = ~1, id = ~ambulance, strata = ~ESA,
+                    nest = TRUE)
 
-# convert to BRR replicate weights
+# ------------------------------------------------------------------
+# Test BRR replicate weights
+# ------------------------------------------------------------------
 scd2brr <- as.svrepdesign(scdnofpc, type="BRR", compress = FALSE)
-scd2fay <- as.svrepdesign(scdnofpc, type="Fay",fay.rho=0.3)
-# convert to JKn weights
-scd2jkn <- as.svrepdesign(scdnofpc, type="JKn")
-
-# convert to JKn weights with finite population correction
-scd2jknf <- as.svrepdesign(scddes, type="JKn")
-
-## with user-supplied hadamard matrix
-scd2brr1 <- as.svrepdesign(scdnofpc, type="BRR", hadamard.matrix=paley(11))
-
 
 scd2brr_srvyr <- scd2brr$repweights %>%
   unclass() %>% as.data.frame() %>%
@@ -32,7 +24,110 @@ scd2brr_srvyr <- scd2brr$repweights %>%
                     scale = scd2brr$scale, rscales = scd2brr$rscales,
                     mse = scd2brr$mse, weights = weights)
 
-test_that("design_survey_rep works when using BRR method of replicate weights",
-          expect_equal(svymean(~alive, scd2brr_srvyr),
-                       svymean(~alive, scd2brr)))
+out_survey <- svymean(~alive, scd2brr)
+out_srvyr <- scd2brr_srvyr %>%
+  summarize(alive = survey_mean(alive))
 
+test_that("design_survey_rep works when using BRR method of replicate weights",
+          expect_equal(c(out_survey[[1]], sqrt(attr(out_survey, "var"))),
+                       c(out_srvyr[[1, 1]], out_srvyr[[1, 2]])))
+
+
+# ------------------------------------------------------------------
+# Test Fay replicate weights
+# ------------------------------------------------------------------
+scd2fay <- as.svrepdesign(scdnofpc, type="Fay", fay.rho = 0.3, compress = FALSE)
+
+scd2fay_srvyr <- scd2fay$repweights %>%
+  unclass() %>% as.data.frame() %>%
+  setNames(paste0("rep", 1:4)) %>%
+  dplyr::bind_cols(scd) %>%
+  mutate(weights = 1) %>%
+  design_survey_rep(repweights = starts_with("rep"),
+                    type = "Fay", rho = 0.3,
+                    scale = scd2fay$scale, rscales = scd2fay$rscales,
+                    mse = scd2fay$mse, weights = weights)
+
+out_survey <- svymean(~alive, scd2fay)
+out_srvyr <- scd2fay_srvyr %>%
+  summarize(alive = survey_mean(alive))
+
+
+test_that("design_survey_rep works when using Fay method of replicate weights",
+          expect_equal(c(out_survey[[1]], sqrt(attr(out_survey, "var"))),
+                       c(out_srvyr[[1, 1]], out_srvyr[[1, 2]])))
+
+# ------------------------------------------------------------------
+# Test JKn replicate weights
+# ------------------------------------------------------------------
+scd2jkn <- as.svrepdesign(scdnofpc, type = "JKn", compress = FALSE)
+
+scd2jkn_srvyr <- scd2jkn$repweights %>%
+  unclass() %>% as.data.frame() %>%
+  setNames(paste0("rep", 1:6)) %>%
+  dplyr::bind_cols(scd) %>%
+  mutate(weights = 1) %>%
+  design_survey_rep(repweights = starts_with("rep"),
+                    type = "JKn",
+                    scale = scd2jkn$scale, rscales = scd2jkn$rscales,
+                    weights = weights)
+
+out_survey <- svymean(~alive, scd2jkn)
+out_srvyr <- scd2jkn_srvyr %>%
+  summarize(alive = survey_mean(alive))
+
+
+test_that("design_survey_rep works when using JKn method of replicate weights",
+          expect_equal(c(out_survey[[1]], sqrt(attr(out_survey, "var"))),
+                       c(out_srvyr[[1, 1]], out_srvyr[[1, 2]])))
+
+
+# ------------------------------------------------------------------
+# Test JKn replicate weights with fpc
+# ------------------------------------------------------------------
+scd2jknf <- as.svrepdesign(scddes, type = "JKn", compress = FALSE)
+
+scd2jknf_srvyr <- scd2jknf$repweights %>%
+  unclass() %>% as.data.frame() %>%
+  setNames(paste0("rep", 1:6)) %>%
+  dplyr::bind_cols(scd) %>%
+  mutate(weights = 1) %>%
+  design_survey_rep(repweights = starts_with("rep"),
+                    type = "JKn",
+                    scale = scd2jknf$scale, rscales = scd2jknf$rscales,
+                    weights = weights)
+
+out_survey <- svymean(~alive, scd2jknf)
+out_srvyr <- scd2jknf_srvyr %>%
+  summarize(alive = survey_mean(alive))
+
+
+test_that("design_survey_rep works when using JKn method of replicate weights",
+          expect_equal(c(out_survey[[1]], sqrt(attr(out_survey, "var"))),
+                       c(out_srvyr[[1, 1]], out_srvyr[[1, 2]])))
+
+
+# ------------------------------------------------------------------
+# Test BRR with user-supplied hadamard matrix
+# ------------------------------------------------------------------
+scd2brr1 <- as.svrepdesign(scdnofpc, type="BRR", hadamard.matrix = paley(11),
+                           compress = FALSE)
+
+scd2brr1_srvyr <- scd2brr1$repweights %>%
+  unclass() %>% as.data.frame() %>%
+  setNames(paste0("rep", 1:12)) %>%
+  dplyr::bind_cols(scd) %>%
+  mutate(weights = 1) %>%
+  design_survey_rep(repweights = starts_with("rep"),
+                    type = "JKn",
+                    scale = scd2brr1$scale, rscales = scd2brr1$rscales,
+                    weights = weights)
+
+out_survey <- svymean(~alive, scd2brr1)
+out_srvyr <- scd2brr1_srvyr %>%
+  summarize(alive = survey_mean(alive))
+
+
+test_that("design_survey_rep works when using JKn method of replicate weights",
+          expect_equal(c(out_survey[[1]], sqrt(attr(out_survey, "var"))),
+                       c(out_srvyr[[1, 1]], out_srvyr[[1, 2]])))

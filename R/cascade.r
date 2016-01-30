cfa9ff8004ae238d5879b3ff3ed4b6a433e1afdd
gergness <greg.freedman@gmail.com>
2016-01-30 15:15:43 -0600

    Add cascade

---
+++ R/cascade.r
@@ -0,0 +1,68 @@
+#' @export
+cascade_.tbl_svy <- function(.data, ..., .dots, .fill = NA) {
+  summarise_.tbl_svy(.data, ..., .dots)
+}
+
+#' @export
+cascade_.grouped_svy <- function(.data, ..., .dots, .fill = NA) {
+  .dots <- lazyeval::all_dots(.dots, ...)
+
+  groups <- as.character(groups(.data))
+  group_cascade <- lapply(rev(seq_along(groups)), function(x) groups[seq_len(x)])
+  group_cascade[length(group_cascade) + 1] <- ""
+
+  out <- lapply(group_cascade,
+                function(ggg) {
+                  if (ggg[1] != "") {
+                    casc <- summarise_(group_by_(.data, .dots = ggg), .dots = .dots)
+                  } else {
+                    casc <- summarise_(ungroup(.data), .dots = .dots)
+                  }
+
+                  missing_vars <- setdiff(groups, ggg)
+                  if (length(missing_vars) > 0) casc[missing_vars] <- .fill
+
+                  casc
+                })
+
+  out <- dplyr::bind_rows(out)
+  out <- dplyr::arrange_(out, groups)
+  out
+}
+
+#' Summarise multiple values into cascading groups
+#'
+#' \code{cascade} is similar to \code{\link{summarise}}, but calculates
+#' a summary statistics for the total of a group in addition to each group.
+#'
+#' @param .data, tbl A \code{tbl_svy} object
+#' @param ... Name-value pairs of summary functions
+#' @param .fill Value to fill in for group summaries
+#' @param .dots Used to work around non-standard evaluation. See
+#' \code{vignette("nse", package = "dplyr")} for details.
+#'
+#' @examples
+#' library(survey)
+#' data(api)
+#'
+#' dstrata <- apistrat %>%
+#'   as_survey_design(strata = stype, weights = pw)
+#'
+#' dstrata_grp <- dstrata %>%
+#'   group_by(stype)
+#'
+#' dstrata_grp %>%
+#'   cascade(api99 = survey_mean(api99),
+#'             api00 = survey_mean(api00),
+#'             api_diff = survey_mean(api00 - api99))
+#'
+#' @export
+cascade <- function(.data, ..., .fill = NA) {
+  cascade_(.data, .dots = lazyeval::lazy_dots(...), .fill = .fill)
+}
+
+#' @export
+#' @rdname cascade
+cascade_ <- function(.data, ..., .dots, .fill = NA) {
+  UseMethod("cascade_")
+}
---
+++ tests/testthat/test_cascade.r
@@ -0,0 +1,52 @@
+context("Cascade works.")
+
+suppressPackageStartupMessages(library(survey))
+data(api)
+
+
+dstrata_srvyr <- apistrat %>%
+  as_survey(strata = stype, weights = pw)
+
+# 1 group
+cascade_results <- dstrata_srvyr %>%
+  group_by(stype) %>%
+  cascade(api99_mn = survey_mean(api99))
+
+summarize_results <- dplyr::bind_rows(
+  dstrata_srvyr %>%
+    group_by(stype) %>%
+    summarize(api99_mn = survey_mean(api99)),
+  dstrata_srvyr %>%
+    summarize(api99_mn = survey_mean(api99))
+)
+
+test_that("cascade works for 1 group",
+          expect_equal(cascade_results, summarize_results))
+
+# 2 groups
+cascade_results <- dstrata_srvyr %>%
+  group_by(stype, awards) %>%
+  cascade(api99_mn = survey_mean(api99))
+
+summarize_results <- dplyr::bind_rows(
+  dstrata_srvyr %>%
+    group_by(stype, awards) %>%
+    summarize(api99_mn = survey_mean(api99)),
+  dstrata_srvyr %>%
+    group_by(stype) %>%
+    summarize(api99_mn = survey_mean(api99)),
+  dstrata_srvyr %>%
+    summarize(api99_mn = survey_mean(api99))
+) %>% dplyr::arrange(stype, awards)
+
+test_that("cascade works for 1 group",
+          expect_equal(cascade_results, summarize_results))
+
+
+# .fill works
+test_that(".fill works",
+          expect_equal(dstrata_srvyr %>%
+                         group_by(stype) %>%
+                         cascade(api99_mn = survey_mean(api99), .fill = "X") %>%
+                         .$stype,
+                       c("E", "H", "M", "X")))

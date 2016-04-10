dbf76d8e2334d3b264c90bc0ec943eb72ee915c4
gergness <greg.freedman@gmail.com>
2016-04-10 11:13:44 -0500

    Remove mean from ratio / quantile test file to decrease redundancy.

--- tests/testthat/test_survey_statistics.r
+++ tests/testthat/test_survey_statistics.r
@@ -1,4 +1,4 @@
-context("Quick tests for added summary stats (ratio / quantile)")
+context("Quick tests for summary stats (ratio / quantile)")
 
 library(srvyr)
 library(survey)
@@ -99,45 +99,36 @@ test_that(
 
 
 out_srvyr <- dstrata %>%
-  summarize(mn = survey_mean(api00, vartype = "ci", level = 0.9),
-            ratio = survey_ratio(api00, api99, vartype = "ci", level = 0.9),
+  summarize(ratio = survey_ratio(api00, api99, vartype = "ci", level = 0.9),
             mdn = survey_median(api00, vartype = "ci", level = 0.9)) %>%
-  select(-mn, -ratio, -mdn_q50) %>%
+  select(-ratio, -mdn_q50) %>%
   unlist()
 
-mn <- svymean(~api00, dstrata)
-mn <- confint(mn, level = 0.9, df = degf(dstrata))
 ratio <- svyratio(~api00, ~api99, dstrata)
 ratio <- confint(ratio, level = 0.9)
 mdn <- svyquantile(~api00, dstrata, quantile = 0.5, ci = TRUE, level = 0.9)
 mdn <- confint(mdn)
-out_survey <- c(mn[1], mn[2], ratio[1], ratio[2], mdn[1], mdn[2])
-names(out_survey) <- c("mn_low", "mn_upp", "ratio_low",
-                       "ratio_upp", "mdn_q50_low", "mdn_q50_upp")
+out_survey <- c(ratio[1], ratio[2], mdn[1], mdn[2])
+names(out_survey) <- c("ratio_low", "ratio_upp", "mdn_q50_low", "mdn_q50_upp")
 
-test_that("mean/median/ratio with CIs respect level parameter (ungrouped)",
+test_that("median/ratio with CIs respect level parameter (ungrouped)",
           expect_equal(out_srvyr, out_survey))
 
 
 suppressWarnings(out_srvyr <- dstrata %>%
   group_by(stype) %>%
-  summarize(mn = survey_mean(api00, vartype = "ci", level = 0.9),
-            ratio = survey_ratio(api00, api99, vartype = "ci", level = 0.9),
+  summarize(ratio = survey_ratio(api00, api99, vartype = "ci", level = 0.9),
             mdn = survey_median(api00, vartype = "ci", level = 0.9)) %>%
-  select(-mn, -ratio, -mdn_q50, -stype)
+  select(-ratio, -mdn_q50, -stype)
 )
 
-mn <- svyby(~api00, ~stype, dstrata, svymean)
-mn <- confint(mn, level = 0.9, df = degf(dstrata))
 ratio <- svyby(~api00, ~stype, denominator = ~api99, dstrata, svyratio)
 ratio <- confint(ratio, level = 0.9)
 suppressWarnings(mdn <- svyby(~api00, ~stype, dstrata, svyquantile,
                               quantile = 0.5, ci = TRUE, level = 0.90))
 mdn <- confint(mdn, level = 0.9)
-out_survey <- dplyr::bind_cols(data.frame(mn), data.frame(ratio),
-                               data.frame(mdn))
-names(out_survey) <- c("mn_low", "mn_upp", "ratio_low", "ratio_upp",
-                       "mdn_q50_low", "mdn_q50_upp")
+out_survey <- dplyr::bind_cols(data.frame(ratio), data.frame(mdn))
+names(out_survey) <- c("ratio_low", "ratio_upp", "mdn_q50_low", "mdn_q50_upp")
 
-test_that("mean/median/ratio with CIs respect level parameter (ungrouped)",
+test_that("median/ratio with CIs respect level parameter (ungrouped)",
           expect_equal(out_srvyr, out_survey))

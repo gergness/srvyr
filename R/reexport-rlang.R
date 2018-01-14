494e094c5daf094b4753b3d1bfe1bb57e0043060
gergness <greg.freedman@gmail.com>
2018-01-14 13:33:40 -0600

    Update rlang exports

--- R/reexport-rlang.R
+++ R/reexport-rlang.R
@@ -1,12 +1,16 @@
 # Follow dplyr's lead on which functions to export
-#' @export
-rlang::quo
 
-#' @export
-rlang::quos
-
-#' @export
-rlang::enquo
-
-#' @export
-rlang::quo_name
+#' Tidy eval helpers from rlang
+#'
+#' These functions are used for tidy evaluation, introduced in rlang
+#' and dplyr 0.7. See the programming vignette from dplyr or
+#' <http://rlang.tidyverse.org/articles/tidy-evaluation.html>
+#' for more details.
+#'
+#' @name rlang-tidyeval
+#' @aliases quo quos enquo quo_name sym syms expr exprs enexpr
+#' @export quo quos enquo quo_name
+#' @export sym syms
+#' @export expr exprs enexpr
+#' @keywords internal
+NULL

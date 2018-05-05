dc38a83049c018de94b46a5908db3a4339f7f904
gergness <greg.freedman@gmail.com>
2018-05-05 10:09:09 -0500

    Update pkgdown for 0.3.2 release

--- dev-misc/pkgdown_script.R
+++ dev-misc/pkgdown_script.R
@@ -21,11 +21,11 @@ purrr::walk(all_html_files, function(x) {
   text <- readr::read_lines(file.path("docs", x))
   depth <- stringr::str_count(x, "/")
   img_loc <- paste0(paste(rep("../", depth), collapse = ""), "tools/logo.png")
-  navbar_brand_line <- which(stringr::str_detect(text, stringr::coll("<a class=\"navbar-brand\"")))
+  navbar_brand_line <- which(stringr::str_detect(text, stringr::coll("<span class=\"navbar-brand\"")))
   if (length(navbar_brand_line) == 0) return(TRUE)
   text[navbar_brand_line] <- paste(
     text[navbar_brand_line],
-    paste0("<img src=\"", img_loc, "\" height=\"50\" width=\"50\" align=\"left\" display=\"block\" style=\"margin: 5px 20px 5px 0px\">"),
+    paste0("<img src=\"", img_loc, "\" height=\"50\" width=\"50\" align=\"left\" display=\"block\" style=\"margin: -15px 10px 0px 0px\">"),
     sep = "\n"
   )
   readr::write_lines(text, file.path("docs", x))

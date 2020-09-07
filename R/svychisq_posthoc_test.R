00f52368619b81a3eb74cfa013cc48a538367573
zackarno <zackarno@gmail.com>
2020-09-07 15:08:02 +0600

    more seamlessly deal with Na/NaN p values in batch processes

--- R/svychisq_posthoc_test.R
+++ R/svychisq_posthoc_test.R
@@ -29,53 +29,91 @@
 #' @export
 
 svychisq_posthoc_test <-
-  function(design, independent_var, dependent_var,
+  function(design, independent_var=ind_temp, dependent_var=dep_temp,
            method = "bonferroni",
            round = 4,
            alpha = 0.05,
            ...) {
     # Perform the chi square test and save the residuals
     svy_chisq_result<-survey::svychisq(formula = formula(paste0("~",independent_var,"+",dependent_var)),design=design)
-    stdres<-svy_chisq_result$stdres
 
-    # Calculate the chi square values based on the residuls
-    chi_square_values <- stdres ^ 2
-    # Get the p values for the chi square values
-    p_values <- pchisq(chi_square_values, 1, lower.tail = FALSE)
-    # Adjust the p values with the chosen method
-    adjusted_p_values <- p_values
-    for (i in 1:nrow(adjusted_p_values)) {
-      adjusted_p_values[i, ] <- p.adjust(
-        adjusted_p_values[i, ],
-        method = method,
-        n = ncol(adjusted_p_values) * nrow(adjusted_p_values)
-      )
+    stdres<-svy_chisq_result$stdres
+    if(!is.nan(svy_chisq_result$p.value)){
+
+      # Calculate the chi square values based on the residuls
+      chi_square_values <- stdres ^ 2
+      # Get the p values for the chi square values
+      p_values <- pchisq(chi_square_values, 1, lower.tail = FALSE)
+
+      # Adjust the p values with the chosen method
+      adjusted_p_values <- p_values
+      for (i in 1:nrow(adjusted_p_values)) {
+        adjusted_p_values[i, ] <- p.adjust(
+          adjusted_p_values[i, ],
+          method = method,
+          n = ncol(adjusted_p_values) * nrow(adjusted_p_values)
+        )
+      }
+      # Round the adjusted p values
+
+      adjusted_p_values <- round(adjusted_p_values, digits = round)
+      # Convert stdres and adjusted p values into data frames
+      stdres <- as.data.frame.matrix(stdres) %>% filter(!is.na(.))
+      adjusted_p_values <- as.data.frame.matrix(adjusted_p_values)
+      adjusted_p_values <- adjusted_p_values %>% filter(!is.na(.))
+
+
+      adjusted_p_values[adjusted_p_values < alpha] <- paste0(adjusted_p_values[adjusted_p_values < alpha ], "*")
+
+      # Combine residuals and p values into one table
+      results <-
+        as.data.frame(matrix(
+          data = NA,
+          nrow = nrow(adjusted_p_values) * 2,
+          ncol = ncol(adjusted_p_values) + 2
+        ))
+      odd_rows <- seq(1, nrow(results), 2)
+      even_rows <- seq(2, nrow(results), 2)
+      results[odd_rows, c(3:ncol(results))] <- stdres
+      results[even_rows, c(3:ncol(results))] <- adjusted_p_values
+
+      results[odd_rows, 2] <- "residuals"
+      results[even_rows, 2] <- "p_value"
+      colnames <- paste0(dependent_var,"_",dimnames(stdres)[[2]])
+      colnames <- append(colnames, c("indep_var_option", "value"), after = 0)
+      colnames(results) <- colnames
+      rownames <- dimnames(stdres)[[1]]
+      results[odd_rows, 1] <- rownames
+      results[even_rows, 1] <- rownames
+      # Return the results
+      results
     }
-    # Round the adjusted p values
-    adjusted_p_values <- round(adjusted_p_values, digits = round)
-    # Convert stdres and adjusted p values into data frames
-    stdres <- as.data.frame.matrix(stdres)
-    adjusted_p_values <- as.data.frame.matrix(adjusted_p_values)
-    adjusted_p_values[adjusted_p_values < alpha] <- paste0(adjusted_p_values[adjusted_p_values < alpha], "*")
-    # Combine residuals and p values into one table
-    results <-
-      as.data.frame(matrix(
-        data = NA,
-        nrow = nrow(adjusted_p_values) * 2,
-        ncol = ncol(adjusted_p_values) + 2
-      ))
-    odd_rows <- seq(1, nrow(results), 2)
-    even_rows <- seq(2, nrow(results), 2)
-    results[odd_rows, c(3:ncol(results))] <- stdres
-    results[even_rows, c(3:ncol(results))] <- adjusted_p_values
-    results[odd_rows, 2] <- "residuals"
-    results[even_rows, 2] <- "p_value"
-    colnames <- paste0(dependent_var,"_",dimnames(stdres)[[2]])
-    colnames <- append(colnames, c("indep_var", "value"), after = 0)
-    colnames(results) <- colnames
-    rownames <- dimnames(stdres)[[1]]
-    results[odd_rows, 1] <- rownames
-    results[even_rows, 1] <- rownames
-    # Return the results
-    results
+    else{
+      results <-
+        as.data.frame(matrix(
+          data = NA,
+          nrow = nrow(stdres) * 2,
+          ncol = ncol(stdres) + 2
+        ))
+      odd_rows <- seq(1, nrow(results), 2)
+      even_rows <- seq(2, nrow(results), 2)
+
+      results[odd_rows, c(3:ncol(results))] <- stdres
+      results[even_rows, c(3:ncol(results))] <- NA
+      results[odd_rows, 2] <- "residuals"
+      results[even_rows, 2] <- "p_value"
+      colnames <- paste0(dependent_var,"_",dimnames(stdres)[[2]])
+      colnames <- append(colnames, c("indep_var_option", "value"), after = 0)
+      colnames(results) <- colnames
+      rownames <- dimnames(stdres)[[1]]
+      results[odd_rows, 1] <- rownames
+      results[even_rows, 1] <- rownames
+
+
+      # Return the results
+      results
+
+
+    }
+
   }

252b77b2c38fb919bfaff52e418cea9e02ff3d5d
zackarno <zackarno@gmail.com>
2020-09-08 14:54:51 +0600

    switched dep and indep to align with common chisq implementation, also used * instead of _  in pvalue table to make it easier to pivot_longer without potential mistakes due to column names with many _

--- R/svychisq_posthoc_test.R
+++ R/svychisq_posthoc_test.R
@@ -28,26 +28,26 @@
 #' @importFrom stats chisq.test p.adjust pchisq
 #' @export
 #'
-
-quality<-sample(c("sweet","sour","mealy"),1000,replace=T)
-status<- sample(c("1 week old", "1 month old"),1000,replace=T)
-appledf<-srvyr::as_survey(data.frame(apple_status= status, apple_taste=quality))
-library(tidyverse)
-svychisq_posthoc_test(design=appledf, independent_var="apple_taste",dependent_var="apple_status")
-
-
-#simulate datasets
-pt_data<-data.frame(lon=lon, lat=lat, strata=sample(strata_options,1000, replace=TRUE))
-
+# working on example here
+# quality<-sample(c("sweet","sour","mealy"),1000,replace=T)
+# status<- sample(c("1 week old", "1 month old"),1000,replace=T)
+# appledf<-srvyr::as_survey(data.frame(apple_status= status, apple_taste=quality))
+# library(tidyverse)
+# svychisq_posthoc_test(design=appledf, independent_var="apple_taste",dependent_var="apple_status")
+#
+#
+# #simulate datasets
+# pt_data<-data.frame(lon=lon, lat=lat, strata=sample(strata_options,1000, replace=TRUE))
 
 svychisq_posthoc_test <-
-  function(design, independent_var, dependent_var,
+  function(design=hhsvy, independent_var=ind_temp, dependent_var=dep_temp,
            method = "bonferroni",
            round = 4,
            alpha = 0.05,
            ...) {
     # Perform the chi square test and save the residuals
     svy_chisq_result<-survey::svychisq(formula = formula(paste0("~",dependent_var,"+",independent_var)),design=design)
+    # svy_chisq_result<-survey::svychisq(formula = formula(paste0("~",independent_var,"+",dependent_var)),design=design)
 
     stdres<-svy_chisq_result$stdres
     if(!is.nan(svy_chisq_result$p.value)){
@@ -59,12 +59,14 @@ svychisq_posthoc_test <-
 
       # Adjust the p values with the chosen method
       adjusted_p_values <- p_values
-      for (i in 1:nrow(adjusted_p_values)) {
-        adjusted_p_values[i, ] <- p.adjust(
-          adjusted_p_values[i, ],
-          method = method,
-          n = ncol(adjusted_p_values) * nrow(adjusted_p_values)
-        )
+      if(method!="none"){
+        for (i in 1:nrow(adjusted_p_values)) {
+          adjusted_p_values[i, ] <- p.adjust(
+            adjusted_p_values[i, ],
+            method = method,
+            n = ncol(adjusted_p_values) * nrow(adjusted_p_values)
+          )
+        }
       }
       # Round the adjusted p values
 
@@ -75,7 +77,7 @@ svychisq_posthoc_test <-
       adjusted_p_values <- adjusted_p_values %>% filter(!is.na(.))
 
 
-      adjusted_p_values[adjusted_p_values < alpha] <- paste0(adjusted_p_values[adjusted_p_values < alpha ], "*")
+      # adjusted_p_values[adjusted_p_values < alpha] <- paste0(adjusted_p_values[adjusted_p_values < alpha ], "*")
 
       # Combine residuals and p values into one table
       results <-
@@ -91,8 +93,8 @@ svychisq_posthoc_test <-
 
       results[odd_rows, 2] <- "residuals"
       results[even_rows, 2] <- "p_value"
-      colnames <- paste0(dependent_var,"_",dimnames(stdres)[[2]])
-      colnames <- append(colnames, c("indep_var_option", "value"), after = 0)
+      colnames <- paste0(dependent_var,"*",dimnames(stdres)[[2]])
+      colnames <- append(colnames, c("dep_var_option", "value"), after = 0)
       colnames(results) <- colnames
       rownames <- dimnames(stdres)[[1]]
       results[odd_rows, 1] <- rownames
@@ -116,8 +118,8 @@ svychisq_posthoc_test <-
       results[even_rows, c(3:ncol(results))] <- NA
       results[odd_rows, 2] <- "residuals"
       results[even_rows, 2] <- "p_value"
-      colnames <- paste0(dependent_var,"_",dimnames(stdres)[[2]])
-      colnames <- append(colnames, c("indep_var_option", "value"), after = 0)
+      colnames <- paste0(dependent_var,"*",dimnames(stdres)[[2]])
+      colnames <- append(colnames, c("dep_var_option", "value"), after = 0)
       colnames(results) <- colnames
       rownames <- dimnames(stdres)[[1]]
       results[odd_rows, 1] <- rownames
@@ -135,4 +137,101 @@ svychisq_posthoc_test <-
 
   }
 
-
+#
+#
+# svychisq_posthoc_test <-
+#   function(design, independent_var, dependent_var,
+#            method = "bonferroni",
+#            round = 4,
+#            alpha = 0.05,
+#            ...) {
+#     # Perform the chi square test and save the residuals
+#     svy_chisq_result<-survey::svychisq(formula = formula(paste0("~",dependent_var,"+",independent_var)),design=design)
+#
+#     stdres<-svy_chisq_result$stdres
+#     if(!is.nan(svy_chisq_result$p.value)){
+#
+#       # Calculate the chi square values based on the residuls
+#       chi_square_values <- stdres ^ 2
+#       # Get the p values for the chi square values
+#       p_values <- pchisq(chi_square_values, 1, lower.tail = FALSE)
+#
+#       # Adjust the p values with the chosen method
+#       adjusted_p_values <- p_values
+#       for (i in 1:nrow(adjusted_p_values)) {
+#         adjusted_p_values[i, ] <- p.adjust(
+#           adjusted_p_values[i, ],
+#           method = method,
+#           n = ncol(adjusted_p_values) * nrow(adjusted_p_values)
+#         )
+#       }
+#       # Round the adjusted p values
+#
+#       adjusted_p_values <- round(adjusted_p_values, digits = round)
+#       # Convert stdres and adjusted p values into data frames
+#       stdres <- as.data.frame.matrix(stdres) %>% filter(!is.na(.))
+#       adjusted_p_values <- as.data.frame.matrix(adjusted_p_values)
+#       adjusted_p_values <- adjusted_p_values %>% filter(!is.na(.))
+#
+#
+#       adjusted_p_values[adjusted_p_values < alpha] <- paste0(adjusted_p_values[adjusted_p_values < alpha ], "*")
+#
+#       # Combine residuals and p values into one table
+#       results <-
+#         as.data.frame(matrix(
+#           data = NA,
+#           nrow = nrow(adjusted_p_values) * 2,
+#           ncol = ncol(adjusted_p_values) + 2
+#         ))
+#       odd_rows <- seq(1, nrow(results), 2)
+#       even_rows <- seq(2, nrow(results), 2)
+#       results[odd_rows, c(3:ncol(results))] <- stdres
+#       results[even_rows, c(3:ncol(results))] <- adjusted_p_values
+#
+#       results[odd_rows, 2] <- "residuals"
+#       results[even_rows, 2] <- "p_value"
+#       colnames <- paste0(dependent_var,"_",dimnames(stdres)[[2]])
+#       colnames <- append(colnames, c("indep_var_option", "value"), after = 0)
+#       colnames(results) <- colnames
+#       rownames <- dimnames(stdres)[[1]]
+#       results[odd_rows, 1] <- rownames
+#       results[even_rows, 1] <- rownames
+#       # Return the results
+#       results %>%
+#         mutate(ind_var_name=independent_var) %>%
+#         select(ind_var_name,everything())
+#     }
+#     else{
+#       results <-
+#         as.data.frame(matrix(
+#           data = NA,
+#           nrow = nrow(stdres) * 2,
+#           ncol = ncol(stdres) + 2
+#         ))
+#       odd_rows <- seq(1, nrow(results), 2)
+#       even_rows <- seq(2, nrow(results), 2)
+#
+#       results[odd_rows, c(3:ncol(results))] <- stdres
+#       results[even_rows, c(3:ncol(results))] <- NA
+#       results[odd_rows, 2] <- "residuals"
+#       results[even_rows, 2] <- "p_value"
+#       colnames <- paste0(dependent_var,"_",dimnames(stdres)[[2]])
+#       colnames <- append(colnames, c("indep_var_option", "value"), after = 0)
+#       colnames(results) <- colnames
+#       rownames <- dimnames(stdres)[[1]]
+#       results[odd_rows, 1] <- rownames
+#       results[even_rows, 1] <- rownames
+#       results %>%
+#         mutate(ind_var_name=independent_var) %>%
+#         select(ind_var_name,everything())
+#
+#
+#       # Return the results
+#
+#
+#
+#     }
+#
+#   }
+#
+#
